628db0273691f47b5d6f54a7a4bc902338948aee
snowden.xu <snowden.xu@gmail.com>
2025-10-11 13:34:53 +0800

    feat: 降级eslint配置，取消主题

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import { AppRouter } from '@/router';
 import './styles/globals.css';
-import { theme } from './styles/theme';
 
 const root = createRoot(document.body);
 root.render(
-  <ConfigProvider theme={theme}>
+  <ConfigProvider>
     <AppRouter />
   </ConfigProvider>
 );
